5aff41da78bb0668f5b6a5a4e35cdd8e586e0410
Prem chavhan <pchavhan@logiquad.com>
2023-10-20 18:03:51 +0530

    fixes

--- tests/server/build.sh
+++ tests/server/build.sh
@@ -1,5 +1,4 @@
-rm package-lock.json
-rm LICENSE
-rm .gitignore
+rm -f package-lock.json .gitignore LICENSE .editorconfig .gitattributes .gitmodules CODE_OF_CONDUCT.md Makefile install.sh localizely.yml package-lock.json renovate.json
+rm -rf .git cli design docker docs fastlane machine-learning misc mobile nginx web
 cp -rf ./server/* ./
 docker buildx build . --output type=docker,name=elestio4test/immich-server:latest | docker load
